[x] fix scalar transform formula issue.

core/config.py
@@ -309,7 +309,7 @@ class BaseConfig(object):
         epsilon = 0.001
         sign = torch.ones(x.shape).float().to(x.device)
         sign[x < 0] = -1.0
-        output = sign * (torch.sqrt(torch.abs(x / delta) + 1) - 1 + epsilon * x / delta)
+        output = sign * (torch.sqrt(torch.abs(x / delta) + 1) - 1) + epsilon * x / delta
         return output
 
     def inverse_reward_transform(self, reward_logits):